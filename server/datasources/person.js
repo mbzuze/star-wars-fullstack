56157150f160819eaa537b21dab63ce4bd20ea11
Munotidaishe Zuze <mbzuze@gmail.com>
2021-06-26 00:49:14 +0200

    --modified the schema and the server request to account for extra fields

--- server/datasources/person.js
+++ server/datasources/person.js
@@ -26,18 +26,35 @@ class PersonAPI extends RESTDataSource {
   }
 // leaving this inside the class to make the class easier to test
 personReducer(person) {
-    // console.log(this.personReducer(response.results[0].name));
-
-    // let homeworldId = person.homeworld.split("/")[5];
+  // console.log(this.personReducer(response.results[0].name));
+    
+  return person.map(async (person) => {
+    let homeworldId = person.homeworld.split("/")[5];
     // homeworldId = str.homeworldString(":")[5];
-    return person.map(person => 
-        ({
+    let planet = await this.get('planets/' + homeworldId);
+    let planetId = planet.url.split("/")[5];
+          return {
+            userId: person.url.split("/")[5],
             name : person.name,
             height : person.height,
             mass : person.mass,
-            gender : person.gender
-        }));
-  }
+            gender : person.gender,
+            homeworld: {
+              planetId: planetId,
+              name: planet.name,
+              rotation_period: planet.rotation_period,
+              orbital_period: planet.orbital_period,
+              diameter: planet.diameter,
+              climate: planet.climate,
+              gravity: planet.gravity,
+              terrain: planet.terrain,
+              surface_water: planet.surface_water, 
+              population: planet.population,
+              residents: [person]
+            }
+          }
+        });
+}
 
   //get a list of all star wars people
   async getAllPersons() {
@@ -48,11 +65,11 @@ personReducer(person) {
     if( response.next != null){
     let count = 1;
         while (response.next != null){
-                const temp = await this.get('people', {page: count});
-                Array.isArray(temp.results) ? data = data.concat(temp.results) : null ;
-                response = Object.assign(response, temp)
-                console.log(data)
-                count++
+          const temp = await this.get('people', {page: count});
+          Array.isArray(temp.results) ? data = data.concat(temp.results) : null ;
+          response = Object.assign(response, temp)
+          console.log(data)
+          count++
         };
     }else{
         data = response.results;
--- server/schema.js
+++ server/schema.js
@@ -1,35 +1,39 @@
 const { gql } = require('apollo-server')
 const Person  = gql`
 
-type Person {
-    name: String 
-    height: String
-    mass: String
-    gender: String
-    homeworld: [Planets]
-}
- type Planets {
-    name: String 
-    rotation_period: Float 
-    orbital_period: Float 
-    diameter: Float 
-    climate: String 
-    gravity: String 
-    terrain: String 
-    surface_water: Float 
-    population: Float 
-    residents: [Person]
-    films: [Films]
- }
 
- type Films {
+type Films {
+    filmId: ID!
     title: String
-    episode_id: Float 
+    episode_id: String 
     opening_crawl: String
     director: String
     producer: String
  }
 
+type Person {
+    userId: ID!
+    name: String!
+    height: String!
+    mass: String!
+    gender: String!
+    homeworld: Planets!
+}
+
+type Planets {
+   planetId: ID!
+   name: String 
+   rotation_period: String 
+   orbital_period: String 
+   diameter: String 
+   climate: String 
+   gravity: String 
+   terrain: String 
+   surface_water: String 
+   population: String 
+   residents: [Person]
+   films: [Films]
+}
 
 type Query {
     allPersons: [Person]
