d645e11dc0cdf1cfb45b4f4a8165bcdc0298cb9b
Munotidaishe Zuze <mbzuze@gmail.com>
2021-06-26 00:49:41 +0200

    --added a homepage component for styling

---
+++ client/src/components/homepage.component.tsx
@@ -0,0 +1,44 @@
+import "./homepage.styles.scss";
+
+const HomePage = () => {
+
+    return (
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Title stuff</h1> 
+                        <span className="subtitle">sretgf</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Title stuff</h1> 
+                        <span className="subtitle">sretgf</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Title stuff</h1> 
+                        <span className="subtitle">sretgf</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Title stuff</h1> 
+                        <span className="subtitle">sretgf</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Title stuff</h1> 
+                        <span className="subtitle">sretgf</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    );
+};
+
+export default HomePage;
